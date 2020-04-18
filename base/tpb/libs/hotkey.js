90a546401a27f449bc990922b12397c09af7f2bd
zhliner <zhliner@gmail.com>
2020-04-18 16:45:53 +0800

    快捷键支持相同指令的不同处理集。

--- base/tpb/libs/hotkey.js
+++ base/tpb/libs/hotkey.js
@@ -7,8 +7,16 @@
 //////////////////////////////////////////////////////////////////////////////
 //
 //  快捷键映射配置与处理。
+//  支持相同键序列对应到不同的指令标识（由when/not区分）。
 //
-//  此处的配置为默认值，外部用户可以定制修改。
+//  配置结构：{
+//      key:        触发键序列
+//      command:    指令标识序列（空格分隔）
+//      when:       匹配选择器，匹配则触发。可选
+//      not:        不匹配选择器，匹配则不触发。可选
+//  }
+//  说明：
+//  when和not是逻辑和（And）的关系，组合使用可获得精确的限定。
 //
 //
 ///////////////////////////////////////////////////////////////////////////////
@@ -23,15 +31,10 @@ const
 export class HotKey {
 
     constructor() {
-        //
-        // key: evn-keys 快捷键序列
-        // val: {
+        // key: [{
         //      commands: [command],
-        //      when:     {Selector} 匹配选择器，可选
-        //      not:      {Selector} 不匹配选择器（!$.is(...)），可选
-        // }
-        // 注：when和not是And的关系，组合使用可获得精确的限定。
-        //
+        //      match:    {Function|null}
+        // }]
         this._map = new Map();
     }
 
@@ -45,7 +48,12 @@ export class HotKey {
      */
     init( list ) {
         for (const its of list) {
-            this.bind( its.key, its.command, its.when, its.not );
+            this.bind(
+                its.key.trim(),
+                its.command.trim(),
+                its.when && its.when.trim(),
+                its.not && its.not.trim()
+            );
         }
         return this;
     }
@@ -57,18 +65,17 @@ export class HotKey {
      * 注：可用于外部用户配置定制覆盖。
      * @param  {String} key 键序列（单个）
      * @param  {String} cmd 指令标识/序列
-     * @param  {String} when 匹配执行选择器，可选
-     * @param  {String} not 匹配不执行选择器，可选
+     * @param  {String} when 匹配选择器，可选
+     * @param  {String} not 不执行选择器，可选
      * @return {this}
      */
     bind( key, cmd, when, not ) {
+        let _buf = this._map.get(key) || [];
+
         this._map.set(
             key,
-            {
-                commands: cmd.split(__reSpace),
-                match: this._handle(when, not)
-            }
-        );
+            this._addCmd( cmd, when, not, _buf )
+        )
         return this;
     }
 
@@ -81,9 +88,8 @@ export class HotKey {
      * @param  {Element} target 目标元素
      * @return {Boolean}
      */
-    iscmd( cmd, key, target ) {
-        let _cmdx = this._map.get( key );
-        return this._match(_cmdx, target) && _cmdx.commands.includes(cmd);
+    pass( cmd, key, target ) {
+        return this._matches(key, target).includes( cmd );
     }
 
 
@@ -96,14 +102,12 @@ export class HotKey {
      * @return {void}
      */
     fire( key, ev, extra = null ) {
-        let _cmdx = this._map.get(key);
+        let _cmds = this._matches(key, ev.target);
 
-        if ( this._match(_cmdx, ev.target) ) {
-            for (const cmd of _cmdx.commands) {
-                $.trigger( ev.target, cmd, extra, true, true );
-            }
-            ev.preventDefault();
+        for ( const cmd of _cmds ) {
+            $.trigger( ev.target, cmd, extra, true, true );
         }
+        if (_cmds.length) ev.preventDefault();
     }
 
 
@@ -111,14 +115,46 @@ export class HotKey {
 
 
     /**
-     * 元素匹配测试。
-     * 如果没有when设置则为无条件匹配。
-     * @param  {Object2}} cmdx 配置对象 {commands, match}
+     * 获取匹配的指令集。
+     * @param  {String} key 触发键序列
      * @param  {Element} target 目标元素
-     * @return {Boolean}
+     * @return {[command]} 指令标识集
      */
-    _match( cmdx, target ) {
-        return cmdx && ( !cmdx.match || cmdx.match(target) )
+    _matches( key, target ) {
+        let _cms = this._map.get(key),
+            _buf = [];
+
+        if ( _cms ) {
+            _cms.forEach( cm =>
+                (!cm.match || cm.match(target)) && _buf.push(...cm.commands)
+            )
+        }
+        return _buf;
+    }
+
+
+    /**
+     * 添加指令处理对象。
+     * Object: {
+     *      commands: [String],
+     *      match: Function|null
+     * }
+     * 注：match为null表示无限制（无条件通过）。
+     *
+     * @param  {String} cmd 指令标识序列
+     * @param  {String} when 匹配选择器，可选
+     * @param  {String} not 不执行选择器，可选
+     * @param  {[Object]} 指令处理器存储集
+     * @return {[Object]} buf
+     */
+    _addCmd( cmd, when, not, buf ) {
+        buf.push(
+            {
+                commands: cmd.split( __reSpace ),
+                match: this._handle( when, not )
+            }
+        );
+        return buf;
     }
 
 
