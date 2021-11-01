40f4d798981111d601a9abf0a24c7bbf55bdd252
zhliner <zhliner@gmail.com>
2021-11-01 19:51:43 +0800

    将基础库 tQuery 和 Tpb 移出项目，另存为单独的库。本项目中仅引用压缩发行版。

--- base/rollup.config.js
+++ base/rollup.config.js
@@ -3,7 +3,7 @@ import { terser } from 'rollup-plugin-terser'
 export default {
     input: 'main.js',
     output: {
-        file: './main.min.js',
+        file: 'main.min.js',
         format: 'esm',
         banner: '/*! Coolj/Tpb@tQuery v0.2.0 | (c) zhliner@gmail.com 2021.10.27 |  */',
         sourcemapExcludeSources: true,
